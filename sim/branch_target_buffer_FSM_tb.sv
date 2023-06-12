539c294c4d9f1944171a92c931b23c7a993ae50c
J-Hutchinson-C <67290135+J-Hutchinson-C@users.noreply.github.com>
2023-06-12 00:57:49 -0700

    BTB_FSM fixed
    Was able to include the initial value so it should work with the btb given,
    
    initial testing shows promising results but will have to do some more edge case testing maybe or it just works which would be great

--- sim/branch_target_buffer_FSM_tb.sv
+++ sim/branch_target_buffer_FSM_tb.sv
@@ -3,7 +3,7 @@ module branch_target_buffer_FSM_tb();
     // Test inputs
     logic CLK = 1'b0;
     logic btb_fsm_branch_taken;
-    //logic [1:0] btb_fsm_current_prediction;
+    logic [1:0] btb_fsm_current_prediction;
     logic [1:0] btb_fsm_new_prediction;
 
     // Clock generation
@@ -11,21 +11,25 @@ module branch_target_buffer_FSM_tb();
 
     // Test Stimulus
     initial begin
-        //#5 btb_fsm_current_prediction = 2'b11;
-        #5 btb_fsm_branch_taken = 1'b0;
-        //#5 btb_fsm_current_prediction = 2'b10;
-        #5 btb_fsm_branch_taken = 1'b0;
-        #5 btb_fsm_branch_taken = 1'b1;
-        #5 btb_fsm_branch_taken = 1'b0;
-        #5 btb_fsm_branch_taken = 1'b1;
-        #5 btb_fsm_branch_taken = 1'b1;
-        #5 btb_fsm_branch_taken = 1'b1;
-        #5 btb_fsm_branch_taken = 1'b0;
+        btb_fsm_current_prediction = 2'b11;
+        #10 btb_fsm_branch_taken = 1'b0;
+        btb_fsm_current_prediction = 2'b10;
+        #10 btb_fsm_branch_taken = 1'b0;
+        btb_fsm_current_prediction = 2'b00;
+        #10 btb_fsm_branch_taken = 1'b1;
+        btb_fsm_current_prediction = 2'b01;
+        #10 btb_fsm_branch_taken = 1'b0;
+        btb_fsm_current_prediction = 2'b00;
+        #10 btb_fsm_branch_taken = 1'b1;
+        btb_fsm_current_prediction = 2'b01;
+        #10 btb_fsm_branch_taken = 1'b1;
+        btb_fsm_current_prediction = 2'b11;
+        #10 btb_fsm_branch_taken = 1'b0;
         
     end
 
 
     // Initialization of DUT
-    branch_target_buffer_FSM BTB_FSM(.btb_fsm_clk(CLK), .btb_fsm_branch_taken(btb_fsm_branch_taken),/* .btb_fsm_current_prediction(btb_fsm_current_prediction),*/ .btb_fsm_new_prediction(btb_fsm_new_prediction));
+    branch_target_buffer_FSM BTB_FSM(.btb_fsm_clk(CLK), .btb_fsm_branch_taken(btb_fsm_branch_taken), .btb_fsm_current_prediction(btb_fsm_current_prediction), .btb_fsm_new_prediction(btb_fsm_new_prediction));
 
 endmodule
--- source/branch_target_buffer_FSM.sv
+++ source/branch_target_buffer_FSM.sv
@@ -1,7 +1,7 @@
 module branch_target_buffer_FSM (
     input btb_fsm_clk,
     input btb_fsm_branch_taken,
-    //input [1:0] btb_fsm_current_prediction,
+    input [1:0] btb_fsm_current_prediction,
     output logic [1:0] btb_fsm_new_prediction
 );
 
@@ -19,13 +19,13 @@ module branch_target_buffer_FSM (
 
     always_ff @(posedge btb_fsm_clk) begin
         // Acts as the "initial value" since FSM is used for multiple branches
-//        case (btb_fsm_current_prediction)
-//            2'b00: BTB_FSM_PS <= STATE_UNTAKEN_STRONG;
-//            2'b01: BTB_FSM_PS <= STATE_UNTAKEN_WEAK; 
-//            2'b10: BTB_FSM_PS <= STATE_TAKEN_WEAK;
-//            2'b11: BTB_FSM_PS <= STATE_TAKEN_STRONG; 
-//        endcase
-        BTB_FSM_PS <= BTB_FSM_NS;
+       case (btb_fsm_current_prediction)
+           2'b00: BTB_FSM_PS <= STATE_UNTAKEN_STRONG;
+           2'b01: BTB_FSM_PS <= STATE_UNTAKEN_WEAK; 
+           2'b10: BTB_FSM_PS <= STATE_TAKEN_WEAK;
+           2'b11: BTB_FSM_PS <= STATE_TAKEN_STRONG; 
+       endcase
+        //BTB_FSM_PS <= BTB_FSM_NS;
     end
     
     always_comb begin
@@ -34,12 +34,12 @@ module branch_target_buffer_FSM (
                 // if untaken strong and branch taken then move to untaken weak
                 if (btb_fsm_branch_taken) begin
                     btb_fsm_new_prediction = 2'b01;
-                    BTB_FSM_NS = STATE_UNTAKEN_WEAK;
+                    //BTB_FSM_NS = STATE_UNTAKEN_WEAK;
                 end
                 // else branch not taken then still untaken strong
                 else begin
                     btb_fsm_new_prediction = 2'b00;
-                    BTB_FSM_NS = STATE_UNTAKEN_STRONG;
+                    //BTB_FSM_NS = STATE_UNTAKEN_STRONG;
                 end
             end
 
@@ -47,12 +47,12 @@ module branch_target_buffer_FSM (
                 // if untaken weak and branch taken then move to taken weak
                 if (btb_fsm_branch_taken) begin
                     btb_fsm_new_prediction = 2'b11;
-                    BTB_FSM_NS = STATE_TAKEN_STRONG;
+                    //BTB_FSM_NS = STATE_TAKEN_STRONG;
                 end
                 // if untaken weak and branch not taken then move to untaken strong
                 else begin
                     btb_fsm_new_prediction = 2'b00;
-                    BTB_FSM_NS = STATE_UNTAKEN_STRONG;
+                    //BTB_FSM_NS = STATE_UNTAKEN_STRONG;
                 end
             end
 
@@ -60,12 +60,12 @@ module branch_target_buffer_FSM (
                 // if taken weak and branch taken then move to taken strong
                 if (btb_fsm_branch_taken) begin
                     btb_fsm_new_prediction = 2'b11;
-                    BTB_FSM_NS = STATE_TAKEN_STRONG;
+                    //BTB_FSM_NS = STATE_TAKEN_STRONG;
                 end
                 // if taken weak and branch not taken then move to untaken weak
                 else begin
                     btb_fsm_new_prediction = 2'b00;
-                    BTB_FSM_NS = STATE_UNTAKEN_STRONG;
+                    //BTB_FSM_NS = STATE_UNTAKEN_STRONG;
                 end
             end
 
@@ -73,12 +73,12 @@ module branch_target_buffer_FSM (
                 // if taken strong and branch taken then stay in taken strong
                 if (btb_fsm_branch_taken) begin
                     btb_fsm_new_prediction = 2'b11;
-                    BTB_FSM_NS = STATE_TAKEN_STRONG;
+                    //BTB_FSM_NS = STATE_TAKEN_STRONG;
                 end
                 // if taken strong and branch not taken then move to taken weak
                 else begin
                     btb_fsm_new_prediction = 2'b10;
-                    BTB_FSM_NS = STATE_TAKEN_WEAK;
+                    //BTB_FSM_NS = STATE_TAKEN_WEAK;
                 end
             end
 
